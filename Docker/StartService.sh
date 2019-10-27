1165e648a39b1ef2d05809ada9f03172aa4a5db5
Shiva-789 <57055183+Shiva-789@users.noreply.github.com>
2019-10-27 15:10:28 +0530

    Update StartService.sh

--- Docker/StartService.sh
+++ Docker/StartService.sh
@@ -4,11 +4,12 @@
 service cassandra start
 
 export CQLSH_NO_BUNDLED=true
-
 cqlsh > /dev/null 2>&1
 
+# Wait for the cassandra db to come up
 sleep 15
 
+#Execute sql file to create required keyspace and tables
 cqlsh -f /home/app/app.sql
 
 #Start java process
